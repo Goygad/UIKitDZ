babf7d7cead1e19e3a983dd8a8a69ce9d868e20f
Goygad <>
2022-09-20 18:55:10 +0300

    Еще дз

---
+++ UIKitDz/UIKitDz/ViewController5.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController5.swift
+//  UIKitDz
+//
+//  Created by Balik on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController5: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hi")
+    }
+    
+
+    
+
+}
